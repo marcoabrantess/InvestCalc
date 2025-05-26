96686ed75e7c0569619db6bcd5e0c7b6362bccfc
Marco Aurélio <marco.abrantes@ufv.br>
2025-05-26 18:13:29 -0300

    capitalize name

---
+++ invest-calc/app/rent-vs-finance/page.tsx
@@ -0,0 +1,9 @@
+import RentOrBuyForm from '@/components/RentOrBuyForm';
+
+export default function AluguelVsFinanciamentoPage() {
+    return (
+        <main className="min-h-screen bg-gray-50 py-10">
+            <RentOrBuyForm />
+        </main>
+    );
+}
