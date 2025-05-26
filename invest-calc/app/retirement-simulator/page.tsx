e80ecbf5b2b1ef30324ceb83bfe0a3071a7e8604
Marco Aurélio <marco.abrantes@ufv.br>
2025-05-26 18:11:33 -0300

    fix page name

--- invest-calc/app/retirement-simulator/page.tsx
+++ invest-calc/app/retirement-simulator/page.tsx
@@ -1,6 +1,6 @@
-import RetirementForm from '@/components/retirement/RetirementForm';
+import RetirementForm from '@/components/RetirementForm';
 
-export default function SimuladorAposentadoriaPage() {
+export default function retirementSimulatorPage() {
     return (
         <main className="min-h-screen bg-gray-50 py-10">
             <RetirementForm />
