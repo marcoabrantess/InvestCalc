b47153d72aaeb67b500076f47763738107a777c9
Marco Aurélio <marco.abrantes@ufv.br>
2025-05-26 18:14:43 -0300

    capitalize name

--- invest-calc/app/retirement-simulator/page.tsx
+++ invest-calc/app/retirement-simulator/page.tsx
@@ -1,6 +1,6 @@
 import RetirementForm from '@/components/RetirementForm';
 
-export default function retirementSimulatorPage() {
+export default function RetirementSimulatorPage() {
     return (
         <main className="min-h-screen bg-gray-50 py-10">
             <RetirementForm />
